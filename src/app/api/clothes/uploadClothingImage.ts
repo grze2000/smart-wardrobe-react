409afd1875f199c25511c953d773ba2fa8fc5aed
grze2000 <programista333@gmail.com>
2023-07-09 11:58:00 +0200

    update external api calls

--- src/app/api/clothes/uploadClothingImage.ts
+++ src/app/api/clothes/uploadClothingImage.ts
@@ -1,17 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const uploadClothingImage = async (file: File, apiKey?: string) => {
-  const formData = new FormData();
-  formData.append('key', apiKey ?? '6d207e02198a847aa98d0a2a901485a5');
-  formData.append('action', 'upload');
-  formData.append('source', file);
-  formData.append('format', 'json');
+  const formData = new FormData()
+  formData.append('key', apiKey ?? '6d207e02198a847aa98d0a2a901485a5')
+  formData.append('action', 'upload')
+  formData.append('source', file)
+  formData.append('format', 'json')
 
-  const response = await axios.post('https://freeimage.host/api/1/upload', formData, {
+  // Proxy is required to avoid CORS issues when calling the API from the frontend
+  //  ProxyPass /api/images https://freeimage.host
+  //  ProxyPassReverse /api/images https://freeimage.host
+  const response = await axios.post('/api/images/api/1/upload', formData, {
     headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  });
+      'Content-Type': 'multipart/form-data',
+    },
+  })
 
-  return response.data;
-};
+  return response.data
+}
--- src/app/api/weather/getWeather.ts
+++ src/app/api/weather/getWeather.ts
@@ -2,16 +2,36 @@ import { useQuery2 } from 'app/hooks'
 import axios from 'axios'
 import { Clothing } from '../interfaces/Clothes'
 import { OPENWEATHERMAP_API_KEY } from 'app/config/env'
+export interface GeolocationPosition {
+  coords: {
+    latitude: number
+    longitude: number
+  }
+}
 
-const getWeather = () =>
-  // axios({
-  //   method: 'GET',
-  //   url: `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${OPENWEATHERMAP_API_KEY}`,
-  // }).then(({ data }) => data)
-  fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${OPENWEATHERMAP_API_KEY}`, {
-    method: 'GET',
-    mode: 'no-cors',
-  }).then((response) => response.json())
+function getCurrentPosition(options = {}): Promise<GeolocationPosition> {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  })
+}
+
+// Proxy is required to avoid CORS issues when calling the API from the frontend
+//  ProxyPass /api/weather http://api.openweathermap.org/
+//  ProxyPassReverse /api/weather http://api.openweathermap.org/
+const getWeather = async () => {
+  let lat = 52.237049
+  let lon = 21.017532
+  try {
+    const position = await getCurrentPosition()
+    lat = position.coords.latitude
+    lon = position.coords.longitude
+  } finally {
+    return axios({
+      method: 'GET',
+      url: `/api/weather/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_API_KEY}&exclude=minutely,hourly,daily&units=metric&lang=pl`,
+    }).then(({ data }) => data)
+  }
+}
 
 export const useGetWeather = () =>
   useQuery2<Clothing[]>({
